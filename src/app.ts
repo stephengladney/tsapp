117352dacb24df1a8b2de6df2cd18331aefa8852
Stephen Gladney <stephengladney@gmail.com>
2022-09-10 18:54:37 -0400

    install path

--- src/app.ts
+++ src/app.ts
@@ -2,12 +2,12 @@ require("dotenv").config()
 
 import express from "express"
 // import bodyParser from "body-parser"
-import { resolve } from "path"
+import path from "path"
 import routes from "./routes/"
 
 const app = express()
 
-app.use(express.static(resolve(__dirname, "../client/build/")))
+app.use(express.static(path.resolve("build")))
 app.use(routes)
 
 app.listen(Number(process.env.PORT) || 5000, () => {
