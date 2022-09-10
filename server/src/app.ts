757a495236e8bcdd026b9a0e580f309fcc6d9c92
Stephen Gladney <stephengladney@gmail.com>
2022-09-10 12:40:24 -0400

    initialize project

---
+++ server/src/app.ts
@@ -0,0 +1,15 @@
+require("dotenv").config()
+
+import express from "express"
+// import bodyParser from "body-parser"
+import { resolve } from "path"
+// import routes from "./routes/"
+
+const app = express()
+
+app.use(express.static(resolve(__dirname, "../../client/build")))
+// app.use(routes)
+
+app.listen(Number(process.env.PORT) || 5000, () => {
+  console.log("TSApp server is now running!")
+})
