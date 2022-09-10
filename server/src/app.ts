06a7fa175c685bb43f45eeebad3b5792f54258cc
Stephen Gladney <stephengladney@gmail.com>
2022-09-10 13:29:12 -0400

    copy video

--- server/src/app.ts
+++ server/src/app.ts
@@ -3,12 +3,12 @@ require("dotenv").config()
 import express from "express"
 // import bodyParser from "body-parser"
 import { resolve } from "path"
-// import routes from "./routes/"
+import routes from "./routes/"
 
 const app = express()
 
 app.use(express.static(resolve(__dirname, "../../client/build")))
-// app.use(routes)
+app.use(routes)
 
 app.listen(Number(process.env.PORT) || 5000, () => {
   console.log("TSApp server is now running!")
--- server/src/routes/index.ts
+++ server/src/routes/index.ts
@@ -1,1 +1,17 @@
-export default {}
+import { Request, Response, Router } from "express"
+import * as path from "path"
+
+const router = Router()
+
+type RouteHandler = (req: Request, res: Response) => void
+
+const FirstEndpoint: RouteHandler = (req, res) => {
+  res.status(200).send("Hello")
+}
+
+router.get("/endpoint", FirstEndpoint)
+router.get("/*", (_: Request, res: Response) => {
+  res.sendFile(path.resolve(__dirname, "../../../client/build/index.html"))
+})
+
+export default router
