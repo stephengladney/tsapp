379c039a79a1f437cf0d236a080ec0824340f871
Stephen Gladney <stephengladney@gmail.com>
2022-09-10 18:37:30 -0400

    move build to root

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -11,7 +11,7 @@ const FirstEndpoint: RouteHandler = (req, res) => {
 
 router.get("/endpoint", FirstEndpoint)
 router.get("/*", (_: Request, res: Response) => {
-  res.sendFile(path.resolve("client", "build", "index.html"))
+  res.sendFile(path.resolve("build", "index.html"))
 })
 
 export default router
